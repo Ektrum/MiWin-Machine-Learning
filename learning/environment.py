92fdf4610dbc3d3d71b1a7e0ddb554b45ea97af9
CalilQ <calil_queiroz@hotmail.com>
2018-07-10 17:50:35 -0300

    Create environment class skeleton

---
+++ learning/environment.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 10 17:01:12 2018
+
+@author: Calil
+"""
+
+from sharc.main_cli import main
+
+class Environment(object):
+    """
+    
+    """
+    def __init__():
+        """
+        
+        """
+        pass
+    
+    def perform_action(action_id: int):
+        """
+        
+        """
+        pass
+    
+    def start_simulator():
+        """
+        
+        """
+        pass
+    
+    def collect_reward(state_id: int, reward_id: int):
+        """
+        
+        """
+        pass
